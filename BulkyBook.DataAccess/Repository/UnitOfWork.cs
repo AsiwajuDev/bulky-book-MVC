cb57dfb5f0fbbef7810643b7ed6115e085187c49
Kazeem Asiwaju-Bello <kasiwaju@axamansard.com>
2021-01-13 22:44:12 +0100

    Added IUnitOfWork and UnitOfWork class to wrap the whole Repository

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            StoredProcedure_Call = new StoredProcedure_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public IStoredProcedure_Call StoredProcedure_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
