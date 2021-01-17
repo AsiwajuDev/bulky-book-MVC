0afa781d8d097be51c0815bdb2893ad7f61a5101
Kazeem Asiwaju-Bello <kasiwaju@axamansard.com>
2021-01-17 01:28:28 +0100

    Added Controller and views for CoverType

---
+++ bulky-book-MVC/Areas/Admin/Controllers/CoverTypesController.cs
@@ -0,0 +1,88 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BulkyBookMVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypesController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            CoverTypes coverTypes = new CoverTypes();
+            if(id == null)
+            {
+                //to create
+                return View(coverTypes);
+            }
+            //to edit
+            coverTypes = _unitOfWork.CoverTypes.Get(id.GetValueOrDefault());
+
+            if (coverTypes == null)
+            {
+                return NotFound();
+            }
+            return View(coverTypes);
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.CoverTypes.GetAll();
+            return Json(new { data = allObj });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverTypes coverTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                if(coverTypes.Id == 0)
+                {
+                    _unitOfWork.CoverTypes. Add(coverTypes);                    
+;                }
+                else
+                {
+                    _unitOfWork.CoverTypes.Update(coverTypes);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coverTypes);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.CoverTypes.Get(id);
+            if(objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error While Deleting" });
+            }
+            _unitOfWork.CoverTypes.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+
+    }
+}
